810c0ced71034dd43c401323c4292d16676ff1de
Jonathan Brito <jonathanbrito48@gmail.com>
2025-04-18 13:02:22 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   api/__pycache__/serializers.cpython-312.pyc
    	modified:   api/__pycache__/urls.cpython-312.pyc
    	modified:   api/__pycache__/views.cpython-312.pyc
    	modified:   api/serializers.py
    	modified:   api/urls.py
    	modified:   api/views.py

--- api/views.py
+++ api/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 from rest_framework import viewsets,mixins
 from rest_framework.permissions import IsAuthenticated
 from .models import dim_cliente,dim_produto,dim_tempo_entrega,dim_tempo_venda,dim_tipo_entrega,fato_vendas
-from .serializers import ClienteSerializer
+from .serializers import ClienteSerializer,ProdutoSerializer,TempoEntregaSerializer,TempoVendaSerializer,TipoEntregaSerializer,VendasSerializer
 from .authentication import MD5TokenAuthentication
 
-class ClienteViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet):
+
+class ClienteViewSet(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet):
     queryset = dim_cliente.objects.all()
     serializer_class = ClienteSerializer
     authentication_classes = [MD5TokenAuthentication]
@@ -21,3 +21,82 @@ class ClienteViewSet(
 
     def perform_create(self, serializer):
         serializer.save(empresa=self.request.user)
+
+class ProdutoViewSet(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = dim_produto.objects.all()
+    serializer_class = ProdutoSerializer
+    authentication_classes = [MD5TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return dim_produto.objects.filter(empresa=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(empresa=self.request.user)
+
+
+class TempoEntregaViewSet(mixins.ListModelMixin,
+                          mixins.CreateModelMixin,
+                          mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = dim_tempo_entrega.objects.all()
+    serializer_class = TempoEntregaSerializer
+    authentication_classes = [MD5TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return dim_tempo_entrega.objects.filter(empresa=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(empresa=self.request.user)
+
+
+class TempoVendaViewSet(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        viewsets.GenericViewSet):
+    queryset = dim_tempo_venda.objects.all()
+    serializer_class = TempoVendaSerializer
+    authentication_classes = [MD5TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return dim_tempo_venda.objects.filter(empresa=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(empresa=self.request.user)
+
+
+class TipoEntregaViewSet(mixins.ListModelMixin,
+                         mixins.CreateModelMixin,
+                         mixins.RetrieveModelMixin,
+                         viewsets.GenericViewSet):
+    queryset = dim_tipo_entrega.objects.all()
+    serializer_class = TipoEntregaSerializer
+    authentication_classes = [MD5TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return dim_tipo_entrega.objects.filter(empresa=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(empresa=self.request.user) 
+
+
+class VendaViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = fato_vendas.objects.all()
+    serializer_class = VendasSerializer
+    authentication_classes = [MD5TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return fato_vendas.objects.filter(empresa=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(empresa=self.request.user)       
