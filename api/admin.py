e10883711af2e9a2d3f73b082f0131e674771c14
Jonathan Brito <jonathanbrito48@gmail.com>
2025-04-20 14:06:35 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   api/__pycache__/admin.cpython-312.pyc
    	modified:   api/admin.py

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,7 @@ from django.db import models
 from .models import empresa
 
 class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ('id','client_name','api_token','created_at')
+    list_display = ('id','client_name','api_token','created_at','ativo')
+    list_editable = ('ativo',)
 
 admin.site.register(empresa,EmpresaAdmin)
